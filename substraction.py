8ac047aa4763c3f2193437173c05288a6794d51b
Harshvardhan Mali <harshmali910@gmail.com>
2026-03-18 10:13:12 +0530

    added substraction code

---
+++ substraction.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+num1 = 18
+num2 = 7
+
+sum = num1 + num2
+
+print(f"Sum of {num1} and {num2} is {sum}")
+difference = num1 - num2
+print(f"Difference of {num1} and {num2} is {difference}")
